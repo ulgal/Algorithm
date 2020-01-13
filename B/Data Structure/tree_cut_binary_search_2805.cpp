0d322bd7ccfe0d54c4d3ffa86ddc4bc15b31acc6
ulgal <eloonsang@snu.ac.kr>
2020-01-13 15:06:44 +0900

    remove sort

--- B/Data Structure/tree_cut_binary_search_2805.cpp
+++ B/Data Structure/tree_cut_binary_search_2805.cpp
@@ -5,46 +5,8 @@ using namespace std;
 int arr[MAX_SIZE];
 int N, M;
 
-void show(int k) {
-	for (int i = 0; i < k; ++i) {
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-}
-
-void percolateDown(int i, int n) {
-	int lChild = 2 * i;
-	int rChild = 2 * i + 1;
-	if (lChild <= n) {
-		if ((rChild <= n) && (arr[lChild - 1] < arr[rChild - 1])) {
-			lChild = rChild;
-		}
-		if (arr[i - 1] < arr[lChild - 1]) {
-			int tmp = arr[i - 1];
-			arr[i - 1] = arr[lChild - 1];
-			arr[lChild - 1] = tmp;
-			percolateDown(lChild, n);
-		}
-	}
-}
-void heap_sort() {
-	if (N == 0) {
-		return;
-	}
-	int i, tmp, check_idx, check_val, j;
-	for (i = (N / 2); i > 0; --i) {
-		percolateDown(i, N);
-	}
-	for (i = N; i > 1; --i) {
-		tmp = arr[0];
-		arr[0] = arr[i - 1];
-		arr[i - 1] = tmp;
-		percolateDown(1, i - 1);
-	}
-}
-
-long long binary_search() {
-	long long start = 0, end = arr[N - 1];
+long long binary_search(long long mx) {
+	long long start = 0, end = mx;
 	long long mid;
 	long long cur_max = 0;
 	int i;
@@ -74,12 +36,13 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	int i, t;
+	int max_val = 0;
 	cin >> N >> M;
 	for (i = 0; i < N; ++i) {
 		cin >> arr[i];
+		max_val = (max_val > arr[i]) ? (max_val) : (arr[i]);
 	}
-	heap_sort();
-	cout << binary_search();
+	cout << binary_search((long long)max_val);
 	return 0;
 }
 
